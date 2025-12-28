0a101387e9620d3154d2aa40d9c65c302ed5aa24
aliceincipherland <la.ta.more@protonmail.com>
2025-12-28 13:36:43 -0600

    first commit

---
+++ followtherabbit.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved
+)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        MessageBox(NULL, L"(\\___/)\n(o x o)!", L"Late, Late, Late!", NULL);
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
